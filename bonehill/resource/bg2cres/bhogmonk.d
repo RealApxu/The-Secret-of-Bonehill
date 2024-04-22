43c03b7a254e2a57f4d967497ede012cc4989324
Алексей А <alex021998@gmail.com>
2024-04-22 22:34:11 +1000

    BG:EE Compatibility

---
+++ bonehill/resource/bg2cres/bhogmonk.d
@@ -0,0 +1,21 @@
+BEGIN BHOGMONK
+
+IF ~RandomNum(4,1)~ THEN BEGIN 0 // from:
+  SAY @812
+  IF ~~ THEN EXIT
+END
+
+IF ~RandomNum(4,2)~ THEN BEGIN 1 // from:
+  SAY @813
+  IF ~~ THEN EXIT
+END
+
+IF ~RandomNum(4,3)~ THEN BEGIN 2 // from:
+  SAY @814
+  IF ~~ THEN EXIT
+END
+
+IF ~RandomNum(4,4)~ THEN BEGIN 3 // from:
+  SAY @815
+  IF ~~ THEN EXIT
+END
